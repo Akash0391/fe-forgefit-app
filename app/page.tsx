84953481ad479127692f852d81febc5b058db0a1
Akash03 <your-email@example.com>
2025-11-09 12:22:26 +0530

    chore: update dependencies and enhance global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
-    <h1 className="text-3xl text-center text-blue-500 font-bold">
-      ForgeFit
-    </h1>
+    <>
+      <h1 className="text-3xl text-center text-blue-500 font-bold">
+        ForgeFit
+      </h1>
+      <div className="flex justify-center items-center"> <Button variant="default" className="bg-blue-500 text-white hover:bg-blue-600">Get Started</Button> </div>
+    </>
   );
 };
 
